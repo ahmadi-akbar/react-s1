f053bd20c0828799dba6fac1aaac9d8ed29be6a1
Akbar Ahmadi <ahmadi.akbar.1996@gmail.com>
2021-01-27 21:30:36 +0330

    add craco package, configs

---
+++ src/screen/Redux/Folder/Dir/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import ShoppingCard1 from "../../../../components/ShoppingCard1";
+// import ShoppingCard2 from "../../../../components/ShoppingCard2";
+import ShoppingCard2 from "~/components/ShoppingCard2";
+
+import ShoppingCard3 from "@/BindingScreen";
+import ShoppingCard4 from "../../../../screen/BindingScreen";
+
+export default function ReduxScreen() {
+  const dispatch = useDispatch();
+  const [text, setText] = React.useState();
+
+  const toggle = () => {
+    dispatch({ type: "ToggleSidebar", payload: null });
+  };
+  const toggleHeader = () => {
+    dispatch({ type: "ToggleHeader", payload: null });
+  };
+
+  const setHeader = () => {
+    dispatch({ type: "SetHeader", payload: text });
+  };
+
+  return (
+    <div className="container">
+      <h1>Home</h1>
+      <ShoppingCard1 />
+      <ShoppingCard2 newProp time date />
+
+      <button onClick={toggle}>toggle sidebar</button>
+      <button onClick={toggleHeader}>toggleHeader Header</button>
+      <div>
+        <input onChange={(e) => setText(e.target.value)} value={text} />
+        <button onClick={setHeader}>Set Header</button>
+      </div>
+    </div>
+  );
+}
